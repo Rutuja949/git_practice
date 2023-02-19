651b58179a355d6e626658458334e674ca54f6b6
Rutuja Umap <80196441+Rutuja949@users.noreply.github.com>
2023-02-19 19:13:02 +0530

    Update prime.js
    I made changes

--- prime.js
+++ prime.js
@@ -1,5 +1,5 @@
 let num =8;
-<<<<<<< HEAD
+
 let flag = true
 
 for(let i = 2; i<num; i--){
@@ -8,17 +8,4 @@ for(let i = 2; i<num; i--){
 }
 (flag)?console.log("Prime"):console.log("Not prime")
      
-=======
-let count=0;
-for(let i = 0; i<num; i--){
-	if(num%i == 0){
-	count++;
-	}
-	}
-	if(count==2){
-		console.log("Prime")
-     }
-     else{
-         console.log("Not prime")
-}
->>>>>>> 320b30770f3e3215ae2e8f1d6795445b26944470
+
