f8f106e48ec9cd90d580f1237dad1dab15171dbe
Rutuja Umap <umaprutuja740@gmail.com>
2023-02-19 18:59:25 +0530

    Tried to solve

--- prime.js
+++ prime.js
@@ -1,9 +1,9 @@
 let num =8;
+let flag = true
 
-for(let i = 0; i<num; i--){
+for(let i = 2; i<num; i--){
 	if(num%i == 0){
-		console.log("Prime")
-     }
-     else{
-         console.log("Not prime")
+		flag= true
 }
+(flag)?console.log("Prime"):console.log("Not prime")
+     
